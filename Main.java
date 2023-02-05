ce06014d92ceae4a30228d177a89411f913db827
Manish636700 <98324095+Manish636700@users.noreply.github.com>
2023-02-05 21:16:40 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+// class name and file save name same
+import java.util.*;
+public class Main{
+public static void main(String args[])
+{
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    if(n%2 != 0)
+        System.out.println("weird");
+        else if(n>=2 && n<=5)
+            System.out.println("not weird");
+            else if(n>=6 && n<=20)
+                System.out.println("weird");
+            else
+                System.out.println("not weird");
+}
+}
